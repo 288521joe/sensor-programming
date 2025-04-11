16211367f111c6a2241d950bdf113890fc9b9b19
Joseph J <166651739+288521joe@users.noreply.github.com>
2025-04-12 00:06:59 +0530

    Update Digital input instruction Within code.ino

--- Digital input instruction Within code.ino
+++ Digital input instruction Within code.ino
@@ -1,1 +1,9 @@
-
+#define LED1_PIN1  2 // GPIO2
+void setup() {
+    pinMode(LED1_PIN1, OUTPUT);
+}
+void loop() {
+    digitalWrite(LED1_PIN1,HIGH);
+    delay(1000);
+    digitalWrite(LED1_PIN1,LOW);
+    delay(1000);
